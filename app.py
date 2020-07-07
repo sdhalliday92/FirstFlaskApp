954ca5e36fff1ed9f7f8dd88af56295901c85bd1
sdhalliday92 <sdhalliday92@gmail.com>
2020-07-07 10:46:59 +0100

    Created about.html, added content to homepage and added links

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
+
+dummyData = [
+    {
+        "f_name": "Tadas",
+        "l_name": "Vaidotas",
+        "title": "Mr",
+        "content": "Blah blah blah"
+    },
+    {
+        "f_name": "Jim",
+        "l_name": "Jones",
+        "title": "Mr",
+        "content": "More content"
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
-def hello_world():
-    return render_template('homepage.html', title='Homepage')
+def home():
+    return render_template('homepage.html', title='Homepage', posts=dummyData)
 
 
 @app.route('/about')
 def about():
-    return '''
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>About Page</title>
-    </head>
-    <body>
-        <h1>This about page</h1>
-    </body>
-    </html>
-    '''
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
